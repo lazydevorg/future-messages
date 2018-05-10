28c45ce23598129640a2dc18386ad7abf2a11021
Fabio Volpe <volpefabio@gmail.com>
2018-05-10 16:51:27 +0100

    send message on default queue

--- src/main/java/org/lazydev/futuremessages/schedule/MessageSenderJob.java
+++ src/main/java/org/lazydev/futuremessages/schedule/MessageSenderJob.java
@@ -4,12 +4,27 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 
+@EnableBinding(Source.class)
 class MessageSenderJob implements Job {
     private static final Logger log = LoggerFactory.getLogger(MessageSenderJob.class);
 
+    @Autowired
+    private MessageChannel output;
+
     @Override
     public void execute(JobExecutionContext context) {
-        log.debug(context.toString());
+        String payload = context.getMergedJobDataMap().getString("payload");
+        Message<String> message = MessageBuilder.withPayload(payload)
+                .setHeader("scheduleId", context.getTrigger().getKey().getName())
+                .build();
+        output.send(message);
+        log.debug("Message {}", message);
     }
 }
