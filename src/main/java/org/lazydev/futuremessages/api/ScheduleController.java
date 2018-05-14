04407319db17477d883579f488707a1c46871d11
Fabio Volpe <volpefabio@gmail.com>
2018-05-14 12:41:40 +0100

    controller default media type to json

--- src/main/java/org/lazydev/futuremessages/api/ScheduleController.java
+++ src/main/java/org/lazydev/futuremessages/api/ScheduleController.java
@@ -1,6 +1,5 @@
 package org.lazydev.futuremessages.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.lazydev.futuremessages.schedule.MessageScheduler;
 import org.lazydev.futuremessages.schedule.ScheduledJob;
 import org.quartz.SchedulerException;
@@ -8,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,8 +24,8 @@ public class ScheduleController {
         this.scheduler = scheduler;
     }
 
-    @PostMapping(value = "/schedule", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ScheduleResponse> schedule(@RequestBody @Valid Message message) throws SchedulerException, JsonProcessingException {
+    @PostMapping(value = "/schedule")
+    public ResponseEntity<ScheduleResponse> schedule(@RequestBody @Valid Message message) throws SchedulerException {
         ScheduledJob job = scheduler.schedule(message);
         ScheduleResponse response = new ScheduleResponse(job);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
